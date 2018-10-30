e0ac5fe9017f031f4e605e79253fc50d5baaeb0e
polubis <polubis@wp.pl>
2018-10-30 15:51:52 +0100

    Create shared module and al working ok

--- trello/src/app/app.module.ts
+++ trello/src/app/app.module.ts
@@ -4,47 +4,31 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule } from './app-routing-module';
 import { ProjectsComponent } from './components/projects/projects.component';
-import { LogoComponent } from './components/common/logo/logo.component';
-import { ToglerComponent } from './components/common/togler/togler.component';
-import { FormComponent } from './components/common/form/form.component';
-import { ModalComponent } from './components/common/modal/modal.component';
 import { FormService } from './services/form.service';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { RequestService } from './services/request.service';
-import { SpinnerComponent } from './components/common/spinner/spinner.component';
-import { OperationsComponent } from './components/common/operations/operations.component';
 import { OperationsService } from './services/operations.service';
 import { AuthService } from './services/auth.service';
 import { LoggedUserGuard } from './services/logged-user-guard';
 import { NotLoggedUserGuard } from "src/app/services/not-logged-user-guard";
-import { LoggedNavigationComponent } from './components/common/logged-navigation/logged-navigation.component';
-import { PaginationComponent } from './components/common/pagination/pagination.component';
 import { ProjectDetailsComponent } from './components/projects/project-details/project-details.component';
 import { ProjectsService } from "src/app/services/projects.service";
-import { DebounceClick } from "src/app/directives/debounce-click";
+import { SharedModule } from "src/app/components/common/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProjectsComponent,
-    LogoComponent,
-    ToglerComponent,
-    FormComponent,
-    ModalComponent,
-    SpinnerComponent,
-    OperationsComponent,
-    LoggedNavigationComponent,
-    PaginationComponent,
-    ProjectDetailsComponent,
-    DebounceClick
+    ProjectDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [RequestService, FormService, OperationsService, AuthService, LoggedUserGuard, NotLoggedUserGuard,
     ProjectsService],
---
+++ trello/src/app/components/common/shared.module.ts
@@ -0,0 +1,25 @@
+
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormComponent } from "src/app/components/common/form/form.component";
+import { LoggedNavigationComponent } from "src/app/components/common/logged-navigation/logged-navigation.component";
+import { LogoComponent } from "src/app/components/common/logo/logo.component";
+import { ModalComponent } from "src/app/components/common/modal/modal.component";
+import { OperationsComponent } from "src/app/components/common/operations/operations.component";
+import { PaginationComponent } from "src/app/components/common/pagination/pagination.component";
+import { SpinnerComponent } from "src/app/components/common/spinner/spinner.component";
+import { ToglerComponent } from "src/app/components/common/togler/togler.component";
+import { DebounceClick } from "src/app/directives/debounce-click";
+
+@NgModule({
+    declarations: [ FormComponent, LoggedNavigationComponent, 
+    LogoComponent, ModalComponent, OperationsComponent, PaginationComponent, 
+    SpinnerComponent, ToglerComponent, DebounceClick],
+    imports: [CommonModule],
+    exports: [
+        FormComponent, LoggedNavigationComponent, 
+        LogoComponent, ModalComponent, OperationsComponent, PaginationComponent, 
+        SpinnerComponent, ToglerComponent, DebounceClick
+    ]
+})
+export class SharedModule {}
