4d88b540537b776e11ba3064ada8895dd743f5f9
polubis <polubis@wp.pl>
2018-11-04 16:49:13 +0100

    searched bug

--- trello/src/app/components/common/searcher/searcher.component.ts
+++ trello/src/app/components/common/searcher/searcher.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription, Subject } from "rxjs";
-import { debounceTime } from "rxjs/internal/operators/debounceTime";
 import { UsersService } from "src/app/services/users.service";
-
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/fromPromise';
+ 
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 @Component({
   selector: 'app-searcher',
   templateUrl: './searcher.component.html',
@@ -15,10 +22,11 @@ export class SearcherComponent implements OnInit, OnDestroy {
   constructor(private usersService: UsersService) { }
   subscription: Subscription; 
   results: any[] = [];
+
   ngOnInit() {
-    this.subscription = this.typing.pipe(
-      debounceTime(600) 
-    ).subscribe((value: string) => {
+    this.subscription = this.typing
+    .debounceTime(500).distinctUntilChanged()
+    .subscribe((value: string) => {
       this.isSearching = true;
       this.usersService.getUsersByQuery(value).then((response: any[]) => {
         this.results = response;
@@ -30,10 +38,9 @@ export class SearcherComponent implements OnInit, OnDestroy {
   }
 
   startSearch(e){
-    if(e.target.value.length > 3){
+    if(e.target.value.length > 1){
       this.typing.next(e.target.value);
     }
-    
   }
 
   ngOnDestroy(){
